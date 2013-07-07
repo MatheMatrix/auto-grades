6f826be22c87c5368a00ad99552141d90a0c6b67
glraey <glraey@glraey-comp.(none)>
2013-07-07 16:33:54 +0800

    可以自动从教务系统获得成绩页面并下载，在linux下无法自动打开。

---
+++ connJWXT2.py
@@ -0,0 +1,49 @@
+import urllib2
+import cookielib
+import urllib
+from BeautifulSoup import BeautifulSoup
+import re
+import os
+# import subprocess
+
+login = 'http://www.cdjwc.com/jiaowu/Login.aspx'
+jw = 'http://www.cdjwc.com/jiaowu/JWXS/Default.aspx'
+grates = 'http://www.cdjwc.com/jiaowu/JWXS/cjcx/jwxs_cjcx_like.aspx'
+
+if 'connJWXT2.ini' in os.listdir(os.getcwd()):
+	with open('connJWXT2.ini', 'r') as f:
+		username, password = f.read().split('\n')
+else:
+	username = raw_input('Input your ID: \n')
+	password = raw_input('Input your Password: \n')
+
+content = urllib.urlopen(login).read()
+
+soup = BeautifulSoup(content)
+viewstate	= str(soup.findAll(id = re.compile('__VIEWSTATE'))).split()[-2]
+eventvali	= str(soup.findAll(id = re.compile('__EVENTVALIDATION'))).split()[-2]
+# cmdok		= str(soup.findAll(id = re.compile('cmdok')))
+
+viewstate	= viewstate.split('=')[-1][1:][:-1]
+eventvali	= eventvali.split('=')[-1][1:][:-1]
+
+data = {
+	'__VIEWSTATE'		: viewstate,
+	'__EVENTVALIDATION'	: eventvali,
+	'Account'	: username,
+	'PWD'		: password,
+	'cmdok'		: ''
+}
+
+cookies = urllib2.HTTPCookieProcessor()
+opener = urllib2.build_opener(cookies)
+urllib2.install_opener(opener)
+
+login = urllib2.urlopen(login,urllib.urlencode(data))
+c = urllib2.urlopen(grates)
+with open('out.htm', 'w') as f:
+	f.write(c.read())
+with open('connJWXT2.ini', 'w') as f:
+	f.write(username + '\n' + password)
+
+os.system('google-chrome out.htm')
